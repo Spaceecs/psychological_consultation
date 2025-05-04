7ccabecdb96ddc59f41f7881eb80ddac7a19562b
spaceecs <azaza2046goi@gmail.com>
2025-05-04 21:12:52 +0300

    rework6

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,3 @@
-// components/Footer.tsx
-
 'use client'
 import {
     FaWhatsapp,
@@ -9,31 +7,31 @@ import {
 
 export default function Footer() {
     return (
-        <footer className="w-full bg-[#563440] text-white py-8 px-25" id="contacts">
+        <footer className="w-full bg-[#563440] text-white py-8 px-6" id="contacts">
             <div className="bg-[#563440] mx-auto grid grid-cols-1 lg:grid-cols-3 gap-8">
 
                 {/* Контакти */}
                 <div className="footer-section">
-                    <h2 className="text-2xl font-semibold">Контакти</h2>
-                    <p className="text-lg">+38 (093) 206-91-97</p>
-                    <a href="mailto:gavriluktetana79@gmail.com" className="text-lg hover:underline">
+                    <h2 className="text-2xl font-semibold text-center">Контакти</h2>
+                    <p className="text-lg text-center">+38 (093) 206-91-97</p>
+                    <a href="mailto:gavriluktetana79@gmail.com" className="text-lg hover:underline text-center block">
                         gavriluktetana79@gmail.com
                     </a>
                 </div>
 
                 {/* Технічна підтримка */}
                 <div className="footer-section">
-                    <h2 className="text-2xl font-semibold">Технічна підтримка</h2>
-                    <p className="text-lg">Ми завжди раді допомогти! Звертайтесь за технічною підтримкою.</p>
-                    <a href="mailto:psihologonlinesup@gmail.com" className="text-lg hover:underline">
+                    <h2 className="text-2xl font-semibold text-center">Технічна підтримка</h2>
+                    <p className="text-lg text-center">Ми завжди раді допомогти! Звертайтесь за технічною підтримкою.</p>
+                    <a href="mailto:psihologonlinesup@gmail.com" className="text-lg hover:underline text-center block">
                         psihologonlinesup@gmail.com
                     </a>
                 </div>
 
                 {/* Соціальні мережі */}
                 <div className="footer-section">
-                    <h2 className="text-2xl font-semibold ">Соціальні мережі</h2>
-                    <div className="flex space-x-4 py-8">
+                    <h2 className="text-2xl font-semibold text-center">Соціальні мережі</h2>
+                    <div className="flex justify-center space-x-4 py-8">
                         <a href="https://wa.me/380956237458" target="_blank" rel="noopener noreferrer" aria-label="WhatsApp" className="text-[#FFFFFF] px-2 transform scale-180">
                             <FaWhatsapp />
                         </a>
@@ -43,13 +41,6 @@ export default function Footer() {
                         <a href="https://t.me/Gavrulyk_T" target="_blank" rel="noopener noreferrer" aria-label="Telegram" className="text-[#FFFFFF] px-2 transform scale-180">
                             <FaTelegram />
                         </a>
-                        {/*<a href="https://facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Facebook" className="text-[#FFFFFF] px-2 transform scale-180">*/}
-                        {/*    <FaFacebookF />*/}
-                        {/*</a>*/}
-                        {/*<a href="https://instagram.com" target="_blank" rel="noopener noreferrer" aria-label="Instagram" className="text-[#FFFFFF] px-2 transform scale-180">*/}
-                        {/*    <FaInstagram />*/}
-                        {/*</a>*/}
-
                     </div>
                 </div>
             </div>
